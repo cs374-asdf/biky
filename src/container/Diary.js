97713f2d906366a46504427916842042b54e7648
retro <sangachoi2103@gmail.com>
2021-05-07 23:36:39 +0900

    test: add dummy text

--- src/container/Diary.js
+++ src/container/Diary.js
@@ -5,6 +5,7 @@ import DiaryComponent from '../component/Diary'
 export default function Diary() {
   return (
     <div>
+      아임소쏘리 벋아이럽유 다 거짓말
       <DiaryComponent />
       <Avatar />
     </div>
