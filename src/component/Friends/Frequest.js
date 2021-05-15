9bee9a199332ecc2bc903c9d7b273a24f05e76ec
Nayoung-Oh <lightsalt540@gmail.com>
2021-05-15 22:55:16 +0900

    edit progressbar for image load, add frequest 받은신청

--- src/component/Friends/Frequest.js
+++ src/component/Friends/Frequest.js
@@ -1,26 +1,111 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
-import CloseIcon from '@material-ui/icons/Close';
-import { IconButton } from '@material-ui/core';
-import Box from '@material-ui/core/Box';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import CloseIcon from "@material-ui/icons/Close";
+import { IconButton, Typography } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import CancelIcon from "@material-ui/icons/Cancel";
+import Avatar from "@material-ui/core/Avatar";
+import Divider from "@material-ui/core/Divider";
+import frlist from "./FrequestData.json";
+
 // https://material-ui.com/components/modal/
 
 const useStyles = makeStyles((theme) => ({
   paper: {
-    position: 'absolute',
-    width: '80%',
+    position: "absolute",
+    width: "80%",
     backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
+    border: "2px solid #000",
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
   },
 }));
 
+function ReceivedElement(props) {
+  return (
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="flex-start"
+      flexDirection="row"
+      width="100%"
+      mb={1}
+      style={{ borderTop: "1px dashed lightgray" }}
+    >
+      <Box flex={1} mr={1}>
+        <Avatar
+          alt={props.name}
+          src={process.env.PUBLIC_URL + props.picture}
+          style={{
+            border: "2px solid lightgray",
+          }}
+        />
+      </Box>
+      <Box flex={2} mr={4}>
+        <Typography variant="body2" color="textPrimary">
+          {props.name}
+        </Typography>
+      </Box>
+      <Box flex={1}>
+        <IconButton
+          style={{ color: "green" }}
+          onClick={() => {
+            props.onAcceptClick(props.oid);
+          }}
+        >
+          <CheckCircleIcon />
+        </IconButton>
+      </Box>
+      <Box flex={1}>
+        <IconButton
+          style={{ color: "red" }}
+          onClick={() => {
+            props.onRejectClick(props.oid);
+          }}
+        >
+          <CancelIcon />
+        </IconButton>
+      </Box>
+    </Box>
+  );
+}
+
 export default function Frequest() {
   const classes = useStyles();
-  const [modalStyle] = React.useState({top: `50%`, left: `50%`,transform: `translate(-50%, -50%)`,});
+  const [modalStyle] = React.useState({
+    top: `50%`,
+    left: `50%`,
+    transform: `translate(-50%, -50%)`,
+  });
   const [open, setOpen] = React.useState(false);
+  const [frequests, setFrequests] = React.useState(frlist);
+
+  const acceptFrequest = (fid) => {
+    console.log("add " + fid);
+    setFrequests(frequests.filter((item) => item.id !== fid));
+  };
+
+  const rejectFrequest = (fid) => {
+    setFrequests(frequests.filter((item) => item.id !== fid));
+    /*     console.log(frequests);
+     */
+  };
+
+  function fillTable(el) {
+    // https://javascript.plainenglish.io/material-ui-icons-and-lists-a98c8ccbdac0
+    return (
+      <ReceivedElement
+        key={el.id}
+        oid={el.id}
+        name={el.name}
+        picture={el.picture}
+        onRejectClick={rejectFrequest}
+        onAcceptClick={acceptFrequest}
+      />
+    );
+  }
 
   const handleOpen = () => {
     setOpen(true);
@@ -29,23 +114,46 @@ export default function Frequest() {
   const handleClose = () => {
     setOpen(false);
   };
-// https://stackoverflow.com/questions/42363198/how-to-add-close-icon-in-material-ui-dialog-header-top-right-corner
+  // https://stackoverflow.com/questions/42363198/how-to-add-close-icon-in-material-ui-dialog-header-top-right-corner
   const body = (
     <div style={modalStyle} className={classes.paper}>
-      <h2 id="simple-modal-title">
-        
-        <Box display="flex" alignItems="center">
-            <Box flexGrow = {1}>Frequest</Box>
-            <Box>
-                <IconButton onClick={handleClose}>
-                    <CloseIcon />
-                    </IconButton>
-                </Box>
+      <Box display="flex" flexDirection="column">
+        <Box flex={1} display="flex" alignItems="center" flexDirection="row">
+          <Box flexGrow={1}>
+            <Typography variant="h4" color="textPrimary">
+              Frequest
+            </Typography>
+          </Box>
+          <Box>
+            <IconButton onClick={handleClose}>
+              <CloseIcon />
+            </IconButton>
+          </Box>
         </Box>
-        </h2>
-      <p id="simple-modal-description">
-        Hello 맹구다 맹구
-      </p>
+        <Box flex={4}>
+          <Typography variant="subtitle1" color="textPrimary">
+            받은 신청
+          </Typography>
+          <Box display="flex" alignItems="center" flexDirection="column">
+            {frequests.length == 0 ? (
+              <Typography variant="body1" color="textPrimary">
+                받은 친구 신청이 없습니다
+              </Typography>
+            ) : (
+              frequests.map(fillTable)
+            )}
+          </Box>
+        </Box>
+        <Divider />
+        <Box flex={4}>
+          <Typography variant="subtitle1" color="textPrimary">
+            신청 보내기
+          </Typography>
+          <Typography variant="subtitle1" color="textPrimary">
+            상아님의 searachbar 넣을 예정
+          </Typography>
+        </Box>
+      </Box>
     </div>
   );
 
--- src/component/Friends/Friends.js
+++ src/component/Friends/Friends.js
@@ -1,32 +1,24 @@
 import React from "react";
-import "./Friends.css";
-import PropTypes from "prop-types";
-import { hslToRgb, makeStyles } from "@material-ui/core/styles";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
+import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
-import Button from "@material-ui/core/Button";
-import ButtonGroup from "@material-ui/core/ButtonGroup";
-import ButtonBase from "@material-ui/core/ButtonBase";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 import Box from "@material-ui/core/Box";
-import { Icon, InlineIcon } from "@iconify/react";
-import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import { Icon } from "@iconify/react";
 import Avatar from "@material-ui/core/Avatar";
 import hatchingChick from "@iconify-icons/twemoji/hatching-chick";
 import frontFacingBabyChick from "@iconify-icons/twemoji/front-facing-baby-chick";
-import LinearProgress from "@material-ui/core/LinearProgress";
 
 import flist from "./FriendData.json";
-import { PlayCircleFilledTwoTone, RestaurantRounded } from "@material-ui/icons";
 // https://material-ui.com/components/lists/
 // https://material-ui.com/components/cards/
 // https://material-ui.com/components/progress/
+// https://material-ui.com/components/accordion/
 
-function FriendListItem(props) {
+function FriendListItemInner(props) {
   var icon_name;
   if (props.level === 1) {
     icon_name = hatchingChick;
@@ -34,56 +26,104 @@ function FriendListItem(props) {
     icon_name = frontFacingBabyChick;
   }
   return (
-    <ButtonBase variant="contained" style={{ width: "100%" }}>
-      <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="flex-start"
-        flexDirection="row"
-        width="100%"
-        mb={1}
-      >
-        <Box flex={1} mr={1}>
-          <Icon style={{ fontSize: 30 }} icon={icon_name} />
-        </Box>
-        <Box flex={1} mr={1}>
-          <Avatar
-            alt={props.name}
-            src={require("" + props.picture).default}
-            style={{
-              border: "2px solid lightgray",
-            }}
-          />
-        </Box>
-        <Box flex={2} mr={2}>
-          <Typography variant="body1" color="textPrimary">
-            {props.name}
-          </Typography>
-        </Box>
-        <Box flex={6}>
-          <LinearProgressV2 value={props.intimacy} />
-        </Box>
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="flex-start"
+      flexDirection="row"
+      width="100%"
+      mb={1}
+    >
+      <Box flex={1} mr={1}>
+        <Icon style={{ fontSize: 30 }} icon={icon_name} />
       </Box>
-    </ButtonBase>
+      <Box flex={1} mr={1}>
+        <Avatar
+          alt={props.name}
+          src={process.env.PUBLIC_URL + props.picture}
+          style={{
+            border: "2px solid lightgray",
+          }}
+        />
+      </Box>
+      <Box flex={2} mr={2}>
+        <Typography variant="body1" color="textPrimary">
+          {props.name}
+        </Typography>
+      </Box>
+      <Box flex={6}>
+        <LinearProgressV2 value={props.intimacy} />
+      </Box>
+    </Box>
   );
 }
 
-FriendListItem.propTypes = {
-  level: PropTypes.number.isRequired,
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  intimacy: PropTypes.number.isRequired,
-};
+function FriendListItem(props) {
+  return (
+    <Accordion>
+      <AccordionSummary
+        style={{ width: "95%" }}
+        expandIcon={<ExpandMoreIcon />}
+      >
+        <FriendListItemInner
+          key={props.id}
+          level={props.level}
+          name={props.name}
+          picture={props.picture}
+          intimacy={props.intimacy}
+        />
+      </AccordionSummary>
+      <AccordionDetails style={{ backgroundColor: "lightgray" }}>
+        <Typography variant="body2" color="textPrimary">
+          Rode with {props.name} for {props.time}min and {props.distance}km
+        </Typography>
+      </AccordionDetails>
+    </Accordion>
+  );
+}
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: "100%",
-    maxWidth: 360,
     backgroundColor: theme.palette.background.paper,
   },
 }));
 
+const ProgressStyles = makeStyles((theme) => ({
+  pro_bar: {
+    backgroundColor: "hsl(0, 0%, 97%)",
+    height: "4px",
+    marginBottom: "12px",
+    marginTop: "12px",
+    position: "relative",
+  },
+
+  progress_bar_inner: {
+    backgroundColor: "hsl(0, 0%, 88%)",
+    display: "block",
+    width: 0,
+    height: "100%",
+    position: "absolute",
+    top: 0,
+    left: 0,
+    backgroundColor: "#ff85ac",
+    transition: "width 1s linear 0s",
+    "&::after": {
+      content: `""`,
+      width: "14px",
+      height: "12px",
+      backgroundImage: `url(${process.env.PUBLIC_URL + "/images/bike.png"})`,
+      backgroundSize: "contain",
+      borderRadius: "50%",
+      position: "absolute",
+      right: "-4px",
+      top: "-8px",
+    },
+  },
+}));
+// https://bestjquery.com/tutorial/progress-bar/demo17/
 function LinearProgressV2(props) {
+  const classes = ProgressStyles();
   return (
     <Box
       display="flex"
@@ -92,11 +132,10 @@ function LinearProgressV2(props) {
       justifyContent="flex-start"
     >
       <Box flex={5}>
-        <div className="pro_bar">
+        <div className={classes.pro_bar}>
           <div
-            className="progress_bar_inner"
+            className={classes.progress_bar_inner}
             style={{
-              backgroundColor: "#ff85ac",
               width: props.value + "%",
             }}
             data-value={props.value}
@@ -113,18 +152,6 @@ function LinearProgressV2(props) {
   );
 }
 
-export default function FriendList() {
-  const classes = useStyles();
-
-  return (
-    <div className={classes.root}>
-      <List component="nav" aria-label="main list">
-        {flist.map(fillTable)}
-      </List>
-    </div>
-  );
-}
-
 function fillTable(el) {
   // https://javascript.plainenglish.io/material-ui-icons-and-lists-a98c8ccbdac0
   return (
@@ -134,6 +161,14 @@ function fillTable(el) {
       name={el.name}
       picture={el.picture}
       intimacy={el.total_intimacy}
+      time={el.spent_time}
+      distance={el.distance}
     />
   );
+}
+
+export default function FriendList() {
+  const classes = useStyles();
+
+  return <div className={classes.root}>{flist.map(fillTable)}</div>;
 }
