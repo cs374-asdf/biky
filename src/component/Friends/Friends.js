606bc8f33c959c1aed17c1dd4d39001af4acffde
Nayoung-Oh <lightsalt540@gmail.com>
2021-05-15 14:55:18 +0900

    make a progress bar with a bike riding person

--- src/component/Friends/Friends.js
+++ src/component/Friends/Friends.js
@@ -1,52 +1,70 @@
-import React from 'react';
-import './Friends.css'
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import Button from '@material-ui/core/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
-import ButtonBase from '@material-ui/core/ButtonBase';
+import React from "react";
+import "./Friends.css";
+import PropTypes from "prop-types";
+import { hslToRgb, makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import ButtonBase from "@material-ui/core/ButtonBase";
 
-import Box from '@material-ui/core/Box';
-import { Icon, InlineIcon } from '@iconify/react';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import hatchingChick from '@iconify-icons/twemoji/hatching-chick';
-import frontFacingBabyChick from '@iconify-icons/twemoji/front-facing-baby-chick';
-import LinearProgress from '@material-ui/core/LinearProgress';
+import Box from "@material-ui/core/Box";
+import { Icon, InlineIcon } from "@iconify/react";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import hatchingChick from "@iconify-icons/twemoji/hatching-chick";
+import frontFacingBabyChick from "@iconify-icons/twemoji/front-facing-baby-chick";
+import LinearProgress from "@material-ui/core/LinearProgress";
 
-import flist from './FriendData.json';
-import { RestaurantRounded } from '@material-ui/icons';
+import flist from "./FriendData.json";
+import { PlayCircleFilledTwoTone, RestaurantRounded } from "@material-ui/icons";
 // https://material-ui.com/components/lists/
 // https://material-ui.com/components/cards/
 // https://material-ui.com/components/progress/
 
-/*
-function ListItemLink(props) {
-  return <ListItem button component="a" {...props} />;
-}
-*/
-function FriendListItem(props){
+function FriendListItem(props) {
   var icon_name;
-  if (props.level === 1){
+  if (props.level === 1) {
     icon_name = hatchingChick;
-  }
-  else{
+  } else {
     icon_name = frontFacingBabyChick;
   }
   return (
-    <Box alignItems="center">
-      <ButtonBase variant="contained" color="primary" style={{width:360,}}>
-        <Icon style={{ fontSize: 30 }} icon={icon_name}/>
-        <Avatar alt={props.name} src={require(""+props.picture).default}/>
-        <LinearProgressWithLabel value={props.intimacy} />
-      </ButtonBase>
-    </Box>
+    <ButtonBase variant="contained" style={{ width: "100%" }}>
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="flex-start"
+        flexDirection="row"
+        width="100%"
+        mb={1}
+      >
+        <Box flex={1} mr={1}>
+          <Icon style={{ fontSize: 30 }} icon={icon_name} />
+        </Box>
+        <Box flex={1} mr={1}>
+          <Avatar
+            alt={props.name}
+            src={require("" + props.picture).default}
+            style={{
+              border: "2px solid lightgray",
+            }}
+          />
+        </Box>
+        <Box flex={2} mr={2}>
+          <Typography variant="body1" color="textPrimary">
+            {props.name}
+          </Typography>
+        </Box>
+        <Box flex={6}>
+          <LinearProgressV2 value={props.intimacy} />
+        </Box>
+      </Box>
+    </ButtonBase>
   );
 }
 
@@ -54,40 +72,46 @@ FriendListItem.propTypes = {
   level: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
   picture: PropTypes.string.isRequired,
-  intimacy: PropTypes.number.isRequired
+  intimacy: PropTypes.number.isRequired,
 };
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: '100%',
+    width: "100%",
     maxWidth: 360,
     backgroundColor: theme.palette.background.paper,
   },
 }));
 
-function LinearProgressWithLabel(props) {
+function LinearProgressV2(props) {
   return (
-    <Box display="flex" alignItems="center">
-      <Box minWidth={60} mr={1}>
-        <LinearProgress variant="determinate" {...props} />
+    <Box
+      display="flex"
+      flexDirection="row"
+      alignItems="center"
+      justifyContent="flex-start"
+    >
+      <Box flex={5}>
+        <div className="pro_bar">
+          <div
+            className="progress_bar_inner"
+            style={{
+              backgroundColor: "#ff85ac",
+              width: props.value + "%",
+            }}
+            data-value={props.value}
+            data-percentage-value={props.value}
+          ></div>
+        </div>
       </Box>
-      <Box minWidth={3}>
-        <Typography variant="body2" color="textSecondary">{`${Math.round(
-          props.value,
-        )}%`}</Typography>
+      <Box flex={1}>
+        <Typography variant="body2" color="textPrimary" align="right">
+          {props.value}%
+        </Typography>
       </Box>
     </Box>
   );
 }
-
-LinearProgressWithLabel.propTypes = {
-  /**
-   * The value of the progress indicator for the determinate and buffer variants.
-   * Value between 0 and 100.
-   */
-  value: PropTypes.number.isRequired,
-};
-
 
 export default function FriendList() {
   const classes = useStyles();
@@ -101,40 +125,15 @@ export default function FriendList() {
   );
 }
 
-function fillTable(el)
-{
-  /*
-  var icon_name;
-  if (el.level_badge === 1){
-    icon_name = hatchingChick;
-  }
-  else{
-    icon_name = frontFacingBabyChick;
-  }
-  */
+function fillTable(el) {
   // https://javascript.plainenglish.io/material-ui-icons-and-lists-a98c8ccbdac0
-  var sentence = "total intimacy: "+el.total_intimacy+", time: "+ el.spent_time +", distance: "+ el.distance;
-    return(
-      
-      <FriendListItem key={el.id} level={el.level_badge} name={el.name} picture={el.picture} intimacy={el.total_intimacy}/>
-      
-    );
+  return (
+    <FriendListItem
+      key={el.id}
+      level={el.level_badge}
+      name={el.name}
+      picture={el.picture}
+      intimacy={el.total_intimacy}
+    />
+  );
 }
-/*
-
-<React.Fragment key={el.id}>
-        <ListItem button>
-        <ListItemIcon>
-          <Icon style={{ fontSize: 30 }} icon={icon_name}/>
-        </ListItemIcon>
-        <ListItemAvatar>
-          <Avatar alt={el.name} src={require(""+el.picture).default}/>
-        </ListItemAvatar>
-        <ListItemText primary = {el.name} />
-        <ListItemAvatar>
-          <LinearProgress variant="determinate" value={el.total_intimacy} />
-        </ListItemAvatar>
-      </ListItem>
-      <Divider variant="inset" component="li" />
-      </React.Fragment>
-      */
