f64cdd6fd1796ebd65ac001a1c9ea8457aba83f7
retro <sangachoi2103@gmail.com>
2021-05-21 00:57:00 +0900

    journal detail

--- src/component/journal/JournalDetail.js
+++ src/component/journal/JournalDetail.js
@@ -1,41 +1,78 @@
-import { getDivs, getMetaphors } from './JournalItem'
+import { Icon, InlineIcon } from "@iconify/react";
+import { getDivs, getMetaphors } from "./JournalItem"
 
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import { CardActions } from '@material-ui/core';
-import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
-import { Link } from 'react-router-dom';
-import PictureList from './PictureList';
-import React from 'react';
-import StaticMap from '../home/StaticMap'
-import dayjs from 'dayjs'
-import { getFriends } from './JournalItem';
-import { makeStyles } from '@material-ui/core/styles';
-import {nullToList} from '../../util/format'
-import {getIconComponent} from '../../util/icon'
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import { CardActions } from "@material-ui/core";
+import CardContent from "@material-ui/core/CardContent";
+import CardHeader from "@material-ui/core/CardHeader";
+import DateComponent from "./DateComponent";
+import IconButton from "@material-ui/core/IconButton";
+import { Link } from "react-router-dom";
+import PictureList from "./PictureList";
+import React from "react";
+import StaticMap from "../home/StaticMap"
+import crossMark from '@iconify-icons/twemoji/cross-mark';
+import dayjs from "dayjs"
+import { getFriends } from "./JournalItem";
+import {getIconComponent} from "../../util/icon"
+import { makeStyles } from "@material-ui/core/styles";
+import {nullToList} from "../../util/format"
+import pencilIcon from '@iconify-icons/twemoji/pencil';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
-    width: '80%',
-    position: 'absolute',
-    maxHeight: '80%',
+    width: "80%",
+    position: "absolute",
+    maxHeight: "80%",
     padding: theme.spacing(2, 4, 3),
-
   },
+
+      modal: {
+        top: "50%",
+        left: "50%",
+        transform: `translate(-50%, -50%)`,
+        position: "absolute",
+        width: "75%",
+        maxHeight: "75vh",
+        overflowY: "scroll",
+        backgroundColor: theme.palette.background.paper,
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(4),
+        outline: "none"
+    },
+
+    header: {
+      display: 'flex',
+      justifyContent: 'space-around',
+      
+    },
+
+    emojiHashtagContainer: {
+
+    },
+
+    friendContainer: {
+
+    },
+    
+    metaphorDistanceTimeContainer: {
+
+    },
+  
 }));
 
 const modalStyle = {
-  top: '50%',
-  left: '50%',
-  position: 'absolute',
-  overflow: 'scroll',
+  top: "50%",
+  left: "50%",
+  position: "absolute",
+  overflow: "scroll",
   transform: `translate(-${50}%, -${50}%)`,
 }
 
 function formatTime(time) {
-  console.log('time: ', time)
-  return dayjs(time).format('YYYY년 M월 D일 H시 m분') // '25/01/2019'
+  console.log("time: ", time)
+  return dayjs(time).format("YYYY년 M월 D일 H시 m분") // "25/01/2019"
 }
 
 function getTime(start, end) {
@@ -56,37 +93,48 @@ export default function JournalDetail({ journal, friends }) {
   console.log(journal.friends)
 
   return (
-    <Card style={modalStyle} className={classes.paper}>
-      <CardHeader
-        title={journal.title} />
+    <Card className={classes.modal}>
+      <CardActions>
+        <IconButton
+          component={Link} to={`/edit/${journal.id}`}
+        >
+          <Icon icon={pencilIcon}/>
+      </IconButton>
+        <IconButton onClick={() => alert("삭제")}>
+          <Icon icon={crossMark}/>
+        </IconButton>
+      </CardActions>
+      
+      <div className={classes.header}>
+        <div> {journal.title} </div>
+        <DateComponent startTime={journal.startTime} endTime={journal.endTime}/>        
+      </div>
+              
+      <CardContent>
+      <div>
+        {emojis}
+      </div>
+      <div>
+        {getDivs(journal.hashtags)}
+      </div>
+      </CardContent>
       <CardContent>{journal.desc}</CardContent>
       {/* TODO 적절한 양식으로 friends 보여주기! object의 배열임에 주의*/}
       {/* <CardContent>{getDivs(journal.friends ? journal.friends : [])}</CardContent> */}
-      <CardContent>{getDivs(journal.hashtags)}</CardContent>
-      <CardContent>{emojis}</CardContent>
-      
-      <CardContent>
-        <PictureList pictures={journal.photos} isEditing={false} />
-      </CardContent>
+
       <CardContent>{journal.distance} km</CardContent>
       <CardContent>
         {getTime(journal.startTime, journal.endTime)}
       </CardContent>
       <CardContent>{journal.weather} </CardContent>
       <CardContent>{getMetaphors(journal.metaphor)} </CardContent>
+
       <CardContent>
         <StaticMap route={journal.route} zoom={15} width={"100%"} height={"300px"} />
       </CardContent>
-      <CardActions>
-        <Button disabled={false}
-          component={Link} to={`/edit/${journal.id}`}
-        >
-          수정
-      </Button>
-        <Button onClick={() => alert("삭제")}>
-          삭제
-        </Button>
-      </CardActions>
+      <CardContent>
+        <PictureList pictures={journal.photos} isEditing={false} />
+      </CardContent>
     </Card >
 
   );
