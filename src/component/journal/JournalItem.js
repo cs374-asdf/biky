54d67e30a492f92a87312f7ba016dea6ace0d7aa
GameStop <kaist2015@kaist.ac.kr>
2021-05-21 02:40:54 +0900

    journal-item-design

--- src/component/journal/JournalItem.js
+++ src/component/journal/JournalItem.js
@@ -1,44 +1,276 @@
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import {Link} from 'react-router-dom';
-import React from 'react';
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import { Link } from "react-router-dom";
+import React from "react";
+
+const Hamburger = "/images/hamburger.png";
+const Taxi = "/images/taxi.png";
+const Tree = "/images/tree.png";
 
 function getDivs(items) {
-  return items.map(item => <div key={item}> {item} </div>)
-
+  return items.map((item) => (
+    <div style={{ display: "inline" }} key={item}>
+      {" "}
+      {item}{" "}
+    </div>
+  ));
 }
 
 function getFriends(friends) {
-  return friends.map(friend => <div> {friend} </div>)
+  return friends.map((friend) => (
+    <div
+      style={{
+        display: "inline",
+      }}
+    >
+      {" "}
+      {friend}{" "}
+    </div>
+  ));
 }
 
-function getMetaphors(metaphor) {
-  return <div>
-    <div> 나무 {metaphor.tree} 그루 </div>
-    <div> 택시 {metaphor.taxi} 번 </div>
-    <div> 햄버거 {metaphor.hamburger} 개 </div>
-  </div>
+function getMetaphor(metaphor, randomIndex) {
+  if (randomIndex === 0) {
+    return (
+      <Card
+        style={{
+          background: "linear-gradient(45deg, #73A15D, #94C25A)",
+          width: "100%",
+          height: "10vh",
+          borderRadius: "15px",
+          padding: "10%",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <img
+            src={Tree}
+            style={{
+              position: "absolute",
+              width: "13%",
+              top: "auto",
+              left: "auto",
+            }}
+          />
+          <Typography
+            variant="body1"
+            style={{
+              fontSize: "1.3em",
+              color: "white",
+              position: "absolute",
+              top: "auto",
+              left: "auto",
+            }}
+          >
+            {metaphor.tree} trees
+          </Typography>
+        </div>
+      </Card>
+    );
+  } else if (randomIndex === 1) {
+    return (
+      <Card
+        style={{
+          background: "linear-gradient(45deg, #A0BFE3, #F0A2B0)",
+          width: "80%",
+          height: "10vh",
+          borderRadius: "15px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "spaceAround",
+            alignItems: "center",
+          }}
+        >
+          <img
+            src={Taxi}
+            style={{
+              position: "absolute",
+              width: "13%",
+              top: "15%",
+              left: "3%",
+            }}
+          />
+          <Typography
+            variant="body1"
+            style={{
+              fontSize: "1.3em",
+              color: "white",
+              position: "absolute",
+              top: "30%",
+              left: "20%",
+            }}
+          >
+            {metaphor.taxi} won
+          </Typography>
+        </div>
+      </Card>
+    );
+  } else {
+    return (
+      <Card
+        style={{
+          background: "linear-gradient(45deg, #EED28B, #DB7E61)",
+          width: "80%",
+          height: "10vh",
+          borderRadius: "15px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "spaceAround",
+            alignItems: "center",
+          }}
+        >
+          <img
+            src={Hamburger}
+            style={{
+              position: "absolute",
+              width: "12%",
+              top: "19%",
+              left: "2%",
+            }}
+          />
+          <Typography
+            variant="body1"
+            style={{
+              fontSize: "1.15em",
+              color: "white",
+              position: "absolute",
+              top: "31%",
+              left: "16%",
+            }}
+          >
+            {metaphor.hamburger} burgers
+          </Typography>
+        </div>
+      </Card>
+    );
+  }
 }
-
-
 
 export default function JournalItem({ journal, openJournal }) {
   return (
-    <Card onClick={() => openJournal(journal)}>
-      <CardHeader
-        title={journal.title} />
-      <CardContent>{journal.desc}</CardContent>
-      <CardContent>{getDivs(journal.friends)}</CardContent>
-      <CardContent>{getDivs(journal.hashtags)}</CardContent>
-      <CardContent>{getDivs(journal.emojis)}</CardContent>
-      <CardContent>{journal.distance} km</CardContent>
-      {/* <CardContent>{journal.time} </CardContent> */}
-      <CardContent>{journal.weather} </CardContent>
-      <CardContent>{getMetaphors(journal.metaphor)} </CardContent>
-      <CardContent>{journal.map} </CardContent>
-    </Card >
+    <Card
+      onClick={() => openJournal(journal)}
+      style={{
+        margin: "35px",
+        padding: "35px",
+      }}
+    >
+      <div
+        style={{
+          background: "#DDDDDD",
+          display: "flex",
+          flexWrap: "wrap",
+          height: "100px",
+        }}
+      >
+        <div
+          style={{
+            width: "50%",
+          }}
+        >
+          {/* startTime하니까 오류남.. */}
+          날짜
+        </div>
+
+        <div
+          style={{
+            marginLeft: "auto",
+          }}
+        >
+          {getDivs(journal.emojis)}
+        </div>
+
+        <div
+          style={{
+            marginTop: "auto",
+            width: "50%",
+            fontSize: "2em",
+          }}
+        >
+          {journal.title}
+        </div>
+        <div
+          style={{
+            marginLeft: "auto",
+            marginTop: "auto",
+          }}
+        >
+          {getDivs(journal.hashtags)}
+        </div>
+      </div>
+
+      <div
+        style={{
+          background: "#BCBCBC",
+          position: "relative",
+          top: "5vw",
+          width: "65%",
+          minHeight: "25vh",
+        }}
+      >
+        {journal.desc}
+      </div>
+
+      <div
+        style={{
+          background: "#CCCCCC",
+          position: "relative",
+          top: "10vw",
+          width: "65%",
+          height: "15vh",
+          display: "flex",
+          flexWrap: "wrap",
+        }}
+      >
+        <div
+          style={{
+            width: "35%",
+          }}
+        >
+          {/* {getMetaphor(journal.metaphor, Math.floor(Math.random() * 3))} */}
+          {getMetaphor(journal.metaphor, 2)}
+        </div>
+
+        <div
+          style={{
+            marginLeft: "auto",
+            display: "flex",
+            flexDirection: "column",
+            flexWrap: "wrap",
+          }}
+        >
+          <div
+            style={{
+              marginLeft: "auto",
+            }}
+          >
+            {getDivs(journal.friends)}
+          </div>
+          <div
+            style={{
+              marginTop: "auto",
+            }}
+          >
+            {journal.distance} km
+          </div>
+        </div>
+      </div>
+
+      {/* <CardContent> {journal.map} </CardContent> */}
+    </Card>
   );
 }
--- src/container/journal/JournalMain.js
+++ src/container/journal/JournalMain.js
@@ -1,14 +1,13 @@
-import Button from '@material-ui/core/Button';
-import FilterIcon from '@material-ui/icons/FilterList';
-import JournalDetail from '../../component/journal/JournalDetail'
-import JournalList from '../../component/journal/JournalList'
-import Modal from '@material-ui/core/Modal';
-import React from 'react';
-import SearchBar from '../../component/common/SearchBar';
-import initJournals from '../../data/journal'
+import Button from "@material-ui/core/Button";
+import FilterIcon from "@material-ui/icons/FilterList";
+import JournalDetail from "../../component/journal/JournalDetail";
+import JournalList from "../../component/journal/JournalList";
+import Modal from "@material-ui/core/Modal";
+import React from "react";
+import SearchBar from "../../component/common/SearchBar";
+import initJournals from "../../data/journal";
 
 export default function JournalMain() {
-
   // firebase subscribe를 해서 journal document가 바뀌면 다시 다운받도록 함
   const [journals, setJournals] = React.useState(initJournals);
 
@@ -25,29 +24,51 @@ export default function JournalMain() {
     setSelected(null);
   };
 
-
   return (
     <div>
-      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <SearchBar/>
-        <Button
-          variant="contained"
-          color="primary"
-          size="large"
-          startIcon={<FilterIcon />}
-        >
-          필터
-      </Button>
+      <div style={{ width: "100%", height: "50px", border: "1px black solid" }}>
+        Journal
+      </div>
+      <div style={{ width: "100%", height: "50px", border: "1px black solid" }}>
+        Nayoung Min
       </div>
 
-      <JournalList journals={journals} openJournal={handleOpen} />
-
-      <Modal
-        open={open}
-        onClose={handleClose}
+      <div
+        style={{
+          height: "calc(100% - 260px)",
+        }}
       >
-        <JournalDetail journal={selected} />
-      </Modal>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            padding: "20px",
+            border: "1px black solid",
+          }}
+        >
+          <SearchBar />
+          <Button
+            variant="contained"
+            color="secondary"
+            size="medium"
+            startIcon={<FilterIcon />}
+          >
+            Filter
+          </Button>
+        </div>
+
+        <div
+          style={{
+            border: "1px black solid",
+          }}
+        >
+          <JournalList journals={journals} openJournal={handleOpen} />
+
+          <Modal open={open} onClose={handleClose}>
+            <JournalDetail journal={selected} />
+          </Modal>
+        </div>
+      </div>
     </div>
   );
-};
+}
