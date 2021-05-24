0ac9a258dcde5de64d051bf8858e42856e177c7d
GameStop <kaist2015@kaist.ac.kr>
2021-05-24 22:48:48 +0900

    이제는 loginInitial이 초기화면이야!

--- src/container/Login.js
+++ src/container/Login.js
@@ -1,77 +1,77 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
-import Box from "@material-ui/core/Box";
-import Button from "@material-ui/core/Button";
-import Container from "@material-ui/core/Container";
-import Grid from "@material-ui/core/Grid";
-import TextField from "@material-ui/core/TextField";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import { styled } from "@material-ui/core/styles";
+import Box from '@material-ui/core/Box'
+import Button from '@material-ui/core/Button'
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
+import TextField from '@material-ui/core/TextField'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+import { styled } from '@material-ui/core/styles'
 
-const logo = "/images/logo.png";
+const logo = '/images/logo.png'
 
 const MyButton = styled(Button)({
-  background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
   border: 0,
   borderRadius: 3,
-  boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
-  color: "white",
-  width: "60vw",
-  height: "40px",
-  padding: "0 30px",
-  marginBottom: "10px",
-});
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  color: 'white',
+  width: '60vw',
+  height: '40px',
+  padding: '0 30px',
+  marginBottom: '10px',
+})
 
 const useStyles = makeStyles((theme) => ({
   background: {
-    background: "linear-gradient(0deg, #FFEDE8, #FFFFF5)",
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    width: "100vw",
-    height: "100vh",
+    background: 'linear-gradient(0deg, #FFEDE8, #FFFFF5)',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    width: '100vw',
+    height: '100vh',
     bottom: 0,
   },
   grid: {
-    margin: "auto",
+    margin: 'auto',
   },
   textField: {
-    width: "60vw",
-    background: "#FFFFFF",
+    width: '60vw',
+    background: '#FFFFFF',
   },
   submit: {
-    marginTop: "3vh",
+    marginTop: '3vh',
   },
   login: {
-    marginBottom: "2vh",
+    marginBottom: '2vh',
   },
   logo: {
     paddingTop: theme.spacing(12),
-    paddingBottom: theme.spacing(10),
+    paddingBottom: theme.spacing(15),
   },
-}));
+}))
 
 export default function Login() {
-  const classes = useStyles();
+  const classes = useStyles()
 
   const [val, setVal] = useState({
-    id: "id123",
-    password: "password123",
-  });
+    id: 'id123',
+    password: 'password123',
+  })
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    alert(`아이디: ${val.id} 비밀번호: ${val.password}`);
-    window.location.href = "/biky/journal";
-  };
+    e.preventDefault()
+    alert(`아이디: ${val.id} 비밀번호: ${val.password}`)
+    window.location.href = '/biky/home'
+  }
 
   const onChange = (e) => {
-    const { name, value } = e.target;
+    const { name, value } = e.target
     setVal({
       ...val,
       [name]: value,
-    });
-  };
+    })
+  }
 
   return (
     <Container className={classes.background}>
@@ -116,5 +116,5 @@ export default function Login() {
         </Grid>
       </Box>
     </Container>
-  );
+  )
 }
